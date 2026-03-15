f779da94f09a4d014ec7f218fb79a428f8dcb698
William McCumstie <william@neudata.co>
2026-03-15 19:39:57 +0000

    Adding resources

--- app/models/resource_manager.rb
+++ app/models/resource_manager.rb
@@ -19,8 +19,56 @@ class ResourceManager
     @planks ||= Resource.new("planks", manager: self)
   end
 
+  def gears
+    @gears ||= Resource.new("gears", manager: self)
+  end
+
+  def scrap_metals
+    @scrap_metals ||= Resource.new("scrap_metals", manager: self)
+  end
+
+  def metal_parts
+    @metal_parts ||= Resource.new("metal_parts", manager: self)
+  end
+
+  def kohlrabi
+    @kohlrabi ||= Resource.new("kohlrabi", manager: self)
+  end
+
+  def pine_resins
+    @pine_resins ||= Resource.new("pine_resins", manager: self)
+  end
+
+  def treated_planks
+    @treated_planks ||= Resource.new("treated_planks", manager: self)
+  end
+
+  def extracts
+    @extracts ||= Resource.new("extracts", manager: self)
+  end
+
+  def explosives
+    @explosives ||= Resource.new("explosives", manager: self)
+  end
+
+  def fireworks
+    @fireworks ||= Resource.new("fireworks", manager: self)
+  end
+
+  def waters
+    @waters ||= Resource.new("waters", manager: self)
+  end
+
+  def badwaters
+    @badwaters ||= Resource.new("badwaters", manager: self)
+  end
+
+  def metal_blocks
+    @metal_blocks ||= Resource.new("metal_blocks", manager: self)
+  end
+
   def all_resources
-    [logs, planks]
+    [logs, planks, gears, scrap_metals, metal_parts, kohlrabi, metal_blocks, pine_resins, treated_planks, waters, badwaters, extracts, explosives, fireworks]
   end
 
   def update_status!
--- spec/support/fake_timberborn.rb
+++ spec/support/fake_timberborn.rb
@@ -4,17 +4,65 @@ require "json"
 class FakeTimberborn < Sinatra::Base
   set :protection, except: :host_routing
   DEFAULT_LEVERS = {
-    "P:logs:S"   => { "name" => "P:logs:S",   "state" => false, "springReturn" => false },
-    "P:logs:M"   => { "name" => "P:logs:M",   "state" => false, "springReturn" => false },
-    "P:planks:S" => { "name" => "P:planks:S", "state" => false, "springReturn" => false },
-    "P:planks:M" => { "name" => "P:planks:M", "state" => false, "springReturn" => false }
+    "P:logs:S"         => { "name" => "P:logs:S",         "state" => false, "springReturn" => false },
+    "P:logs:M"         => { "name" => "P:logs:M",         "state" => false, "springReturn" => false },
+    "P:planks:S"       => { "name" => "P:planks:S",       "state" => false, "springReturn" => false },
+    "P:planks:M"       => { "name" => "P:planks:M",       "state" => false, "springReturn" => false },
+    "P:gears:S"        => { "name" => "P:gears:S",        "state" => false, "springReturn" => false },
+    "P:gears:M"        => { "name" => "P:gears:M",        "state" => false, "springReturn" => false },
+    "P:scrap_metals:S" => { "name" => "P:scrap_metals:S", "state" => false, "springReturn" => false },
+    "P:scrap_metals:M" => { "name" => "P:scrap_metals:M", "state" => false, "springReturn" => false },
+    "P:metal_parts:S"  => { "name" => "P:metal_parts:S",  "state" => false, "springReturn" => false },
+    "P:metal_parts:M"  => { "name" => "P:metal_parts:M",  "state" => false, "springReturn" => false },
+    "P:kohlrabi:S"     => { "name" => "P:kohlrabi:S",     "state" => false, "springReturn" => false },
+    "P:kohlrabi:M"       => { "name" => "P:kohlrabi:M",       "state" => false, "springReturn" => false },
+    "P:metal_blocks:S"   => { "name" => "P:metal_blocks:S",   "state" => false, "springReturn" => false },
+    "P:metal_blocks:M"   => { "name" => "P:metal_blocks:M",   "state" => false, "springReturn" => false },
+    "P:pine_resins:S"    => { "name" => "P:pine_resins:S",    "state" => false, "springReturn" => false },
+    "P:pine_resins:M"    => { "name" => "P:pine_resins:M",    "state" => false, "springReturn" => false },
+    "P:treated_planks:S" => { "name" => "P:treated_planks:S", "state" => false, "springReturn" => false },
+    "P:treated_planks:M" => { "name" => "P:treated_planks:M", "state" => false, "springReturn" => false },
+    "P:waters:S"         => { "name" => "P:waters:S",         "state" => false, "springReturn" => false },
+    "P:waters:M"         => { "name" => "P:waters:M",         "state" => false, "springReturn" => false },
+    "P:badwaters:S"      => { "name" => "P:badwaters:S",      "state" => false, "springReturn" => false },
+    "P:badwaters:M"      => { "name" => "P:badwaters:M",      "state" => false, "springReturn" => false },
+    "P:extracts:S"       => { "name" => "P:extracts:S",       "state" => false, "springReturn" => false },
+    "P:extracts:M"       => { "name" => "P:extracts:M",       "state" => false, "springReturn" => false },
+    "P:explosives:S"     => { "name" => "P:explosives:S",     "state" => false, "springReturn" => false },
+    "P:explosives:M"     => { "name" => "P:explosives:M",     "state" => false, "springReturn" => false },
+    "P:fireworks:S"      => { "name" => "P:fireworks:S",      "state" => false, "springReturn" => false },
+    "P:fireworks:M"      => { "name" => "P:fireworks:M",      "state" => false, "springReturn" => false }
   }.freeze
 
   DEFAULT_ADAPTERS = {
-    "P:logs:L"   => { "name" => "P:logs:L",   "state" => false },
-    "P:logs:H"   => { "name" => "P:logs:H",   "state" => false },
-    "P:planks:L" => { "name" => "P:planks:L", "state" => false },
-    "P:planks:H" => { "name" => "P:planks:H", "state" => false }
+    "P:logs:L"         => { "name" => "P:logs:L",         "state" => false },
+    "P:logs:H"         => { "name" => "P:logs:H",         "state" => false },
+    "P:planks:L"       => { "name" => "P:planks:L",       "state" => false },
+    "P:planks:H"       => { "name" => "P:planks:H",       "state" => false },
+    "P:gears:L"        => { "name" => "P:gears:L",        "state" => false },
+    "P:gears:H"        => { "name" => "P:gears:H",        "state" => false },
+    "P:scrap_metals:L" => { "name" => "P:scrap_metals:L", "state" => false },
+    "P:scrap_metals:H" => { "name" => "P:scrap_metals:H", "state" => false },
+    "P:metal_parts:L"  => { "name" => "P:metal_parts:L",  "state" => false },
+    "P:metal_parts:H"  => { "name" => "P:metal_parts:H",  "state" => false },
+    "P:kohlrabi:L"     => { "name" => "P:kohlrabi:L",     "state" => false },
+    "P:kohlrabi:H"       => { "name" => "P:kohlrabi:H",       "state" => false },
+    "P:metal_blocks:L"   => { "name" => "P:metal_blocks:L",   "state" => false },
+    "P:metal_blocks:H"   => { "name" => "P:metal_blocks:H",   "state" => false },
+    "P:pine_resins:L"    => { "name" => "P:pine_resins:L",    "state" => false },
+    "P:pine_resins:H"    => { "name" => "P:pine_resins:H",    "state" => false },
+    "P:treated_planks:L" => { "name" => "P:treated_planks:L", "state" => false },
+    "P:treated_planks:H" => { "name" => "P:treated_planks:H", "state" => false },
+    "P:waters:L"         => { "name" => "P:waters:L",         "state" => false },
+    "P:waters:H"         => { "name" => "P:waters:H",         "state" => false },
+    "P:badwaters:L"      => { "name" => "P:badwaters:L",      "state" => false },
+    "P:badwaters:H"      => { "name" => "P:badwaters:H",      "state" => false },
+    "P:extracts:L"       => { "name" => "P:extracts:L",       "state" => false },
+    "P:extracts:H"       => { "name" => "P:extracts:H",       "state" => false },
+    "P:explosives:L"     => { "name" => "P:explosives:L",     "state" => false },
+    "P:explosives:H"     => { "name" => "P:explosives:H",     "state" => false },
+    "P:fireworks:L"      => { "name" => "P:fireworks:L",      "state" => false },
+    "P:fireworks:H"      => { "name" => "P:fireworks:H",      "state" => false }
   }.freeze
 
   def self.reset!
